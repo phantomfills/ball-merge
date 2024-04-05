2329c632440ae05b466573725d7ff75b41a2192b
AshwinHub <phantomfills@outlook.com>
2024-04-05 13:30:24 +0100

    🔨introduce catppuccin styling

--- src/client/components/ball/ball-counter.tsx
+++ src/client/components/ball/ball-counter.tsx
@@ -5,6 +5,8 @@ import { Image } from "../ui/image";
 import { images } from "shared/assets";
 import { Text } from "../ui/text";
 import { fonts } from "client/constants/fonts";
+import { Shadow } from "../ui/shadow";
+import { style } from "client/constants/style";
 
 interface BallCounterProps {
 	count: number;
@@ -15,27 +17,28 @@ export function BallCounter({ count }: BallCounterProps) {
 		<Frame
 			size={new UDim2(0, 100, 0, 50)}
 			position={new UDim2(0, 10, 1, -60)}
-			backgroundColor={Color3.fromRGB(0, 5, 23)}
+			backgroundColor={style.overlay}
 			backgroundTransparency={0.5}
 			cornerRadius={new UDim(0, 8)}
 		>
+			<Shadow shadowBlur={1} shadowSize={new UDim2(0, 10, 0, 10)} shadowColor={new Color3(0, 0, 0)} />
 			<Outline
 				innerThickness={1}
 				outerThickness={1}
-				innerColor={Color3.fromRGB(255, 255, 255)}
-				outerColor={Color3.fromRGB(255, 255, 255)}
+				innerColor={style.outline}
+				outerColor={style.outline}
 				innerTransparency={0}
-				outerTransparency={0.5}
-				cornerRadius={new UDim(0, 8)}
+				outerTransparency={0.8}
+				cornerRadius={new UDim(0, 3)}
 			/>
 			<Image size={new UDim2(0, 30, 0, 30)} position={new UDim2(0, 10, 0, 10)} image={images.ui.ball} />
 			<Text
 				size={new UDim2(0, 40, 0, 40)}
 				position={new UDim2(0, 50, 0, 5)}
 				textScaled={true}
-				textColor={Color3.fromRGB(255, 255, 255)}
+				textColor={style.text}
 				text={tostring(count)}
-				font={fonts.inter.medium}
+				font={fonts.robotoMono.regular}
 			/>
 		</Frame>
 	);
---
+++ src/client/constants/style.ts
@@ -0,0 +1,8 @@
+import { Frappe } from "@rbxts/catppuccin";
+
+export const style = {
+	background: Frappe.Base,
+	overlay: Frappe.Overlay0,
+	outline: Frappe.Subtext0,
+	text: Frappe.Text,
+} as const;
