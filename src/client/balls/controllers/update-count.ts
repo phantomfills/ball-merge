2429e176f915b904e617cc69f6bd97a0e7e0162e
phantomfills <phantomfills@outlook.com>
2024-07-19 14:27:37 +0100

    🔨add sfx to ball spawning and merging

--- src/client/balls/controllers/update-count.ts
+++ src/client/balls/controllers/update-count.ts
@@ -1,10 +1,22 @@
 import { CollectionService } from "@rbxts/services";
+import { SOUNDS } from "client/constants/sounds";
 import { producer } from "client/store";
+import { playSound } from "client/utils/sound";
 import { TAGS } from "shared/constants/tags";
+
+let prevCount = 0;
 
 function updateCount() {
 	const count = CollectionService.GetTagged(TAGS.BALL).size();
 	producer.setBallCount(count);
+
+	if (prevCount < count) {
+		playSound(SOUNDS.pop);
+	} else {
+		playSound(SOUNDS.merge);
+	}
+
+	prevCount = count;
 }
 
 export function initCountService() {
--- src/client/constants/sounds.ts
+++ src/client/constants/sounds.ts
@@ -1,4 +1,6 @@
 export const SOUNDS = {
 	tap: "rbxassetid://4499400560",
 	buy: "rbxassetid://8522330758",
+	pop: "rbxassetid://5852470908",
+	merge: "rbxassetid://9116422213",
 } as const;
