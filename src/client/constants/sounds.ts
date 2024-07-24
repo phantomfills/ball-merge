9b7f80744675ddf950f79daf6c3b583c30cbffa2
phantomfills <phantomfills@outlook.com>
2024-07-24 14:44:54 +0100

    🔨change merge sound

--- src/client/constants/sounds.ts
+++ src/client/constants/sounds.ts
@@ -2,5 +2,5 @@ export const SOUNDS = {
 	tap: "rbxassetid://4499400560",
 	buy: "rbxassetid://8522330758",
 	pop: "rbxassetid://5852470908",
-	merge: "rbxassetid://9116422213",
+	merge: "rbxassetid://2609981431",
 } as const;
